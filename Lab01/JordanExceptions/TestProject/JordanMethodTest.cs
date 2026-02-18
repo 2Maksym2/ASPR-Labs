1c5049525df0be6c11319841de1ea7ff413faeaa
2Maksym2 <149327980+2Maksym2@users.noreply.github.com>
2026-02-18 18:26:19 +0200

    AddTests

---
+++ Lab01/JordanExceptions/TestProject/JordanMethodTest.cs
@@ -0,0 +1,36 @@
+using JordanExceptions;
+
+namespace TestProject
+{
+    public class JordanMethodTest
+    {
+        [Fact]
+        public void MatrixSolverTest()
+        {
+            var solver = new JordanMethod();
+
+            double[,] inputMatrix = {
+              { 5.0, -3.0, 7.0 },
+              { -1.0, 4.0, 3.0 },
+              { 6.0 , -2.0 , 5.0 }
+              };
+
+            int col = inputMatrix.GetLength(0);
+
+            double[,] expectedMatrix = {
+            { -0.28, -0.011,  0.398 },
+            { -0.247,  0.183,  0.237 },
+            {  0.237,  0.086, -0.183 }
+};
+              
+            inputMatrix = solver.InvertMatrix(inputMatrix);
+
+            for (int i = 0; i < col; i++) {
+                for (int j = 0; j < col; j++)
+                {
+                    Assert.Equal(expectedMatrix[i, j], inputMatrix[i, j], precision: 2);
+                } 
+            }
+        }
+    }
+}
