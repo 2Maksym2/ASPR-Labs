ab1d9947b14449d7eb6c6200966b6cfa5e277f92
2Maksym2 <149327980+2Maksym2@users.noreply.github.com>
2026-02-18 22:02:21 +0200

    AddMoreTests

---
+++ Lab01/JordanExceptions/JordanExceptions/RankSolver.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JordanExceptions
+{
+    public class RankSolver
+    {
+        private readonly IJordanMethod _jordan;
+        public RankSolver(IJordanMethod jordan) 
+        { 
+          _jordan = jordan;
+        }
+
+
+
+        public int GetSolution(double[,] matrixA, int r, int c) 
+        {
+
+
+            int rank = 0;
+           
+            int limit = Math.Min(r, c);
+
+            {            
+                for (int i = 0; i < limit; i++)           
+                {
+                    double pivot = matrixA[i, i];
+                    if (pivot != 0)
+                    {
+                        matrixA = _jordan.MatrixSolver(matrixA, pivot, i, i);
+                        rank++;
+                    }
+                }
+            }
+           
+
+            return rank;
+        }
+    }
+}
--- Lab01/JordanExceptions/TestProject/LinearSolverTest.cs
+++ Lab01/JordanExceptions/TestProject/LinearSolverTest.cs
@@ -4,23 +4,25 @@ namespace TestProject
 {
     public class LinearSolverTest
     {
-        [Fact]
-        public void GetSolution()
+        [Theory]
+        [InlineData(new double[] { 5, -3, 7, -1, 4, 3, 6, -2, 5 }, 3, new double[] { 13, 13, 12 }, new double[] { 1, 2, 2 })]
+        [InlineData(new double[] {6,2,5,-3,4,-1,1,4,3 }, 3, new double[] { 1, 6, 6 }, new double[] { -1,1,1 })]
+        [InlineData(new double[] {-1,1,1,-1,-2,2,3,-1,3}, 3, new double[] {4,3,2}, new double[] {-1,1,2})]
+        public void GetSolution(double[] Matrix, int n, double[] a, double[] expected)
         {
+
+            double[,] inputMatrix = new double[n, n];
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                    inputMatrix[i, j] = Matrix[i * n + j];
+
             var _jordan = new JordanMethod();
             var _solver = new LinearSolver();
             var _invertor = new MatrixInvertor(_jordan);
 
-            double[,] inputMatrix = {
-              { 6, 2, 5},
-              {-3, 4, -1},
-              {1, 4, 3}
-              };
 
             inputMatrix = _invertor.InvertMatrix(inputMatrix);
-            double[] a = {1, 6, 6};
             int count = a.Length;
-            double[] expected = { -1, 1, 1 };
 
             a = _solver.GetSolution(inputMatrix, a);
 
