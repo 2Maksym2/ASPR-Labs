3be7f281976cc2f3d694b39cb1e6b34f4c79d603
2Maksym2 <149327980+2Maksym2@users.noreply.github.com>
2026-02-26 02:47:12 +0200

    Debug|Tests|Protocol

--- Lab01(PartB)/JordanExceptions/JordanExceptions/SimplexSolver.cs
+++ Lab01(PartB)/JordanExceptions/JordanExceptions/SimplexSolver.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Common;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +10,10 @@ namespace JordanExceptions
 {
     public class SimplexSolver
     {
-        private string _rows { get; set; }
-        private string _columns { get; set; }
+        private string[] _rows { get; set; }
+        private string[] _columns { get; set; }
+
+
         private readonly ISaveProtocol _protocol;
         private readonly IJordanMethod _jordan;
 
@@ -21,23 +25,25 @@ namespace JordanExceptions
 
 
 
-        public void FindInitialFeasibleSolution(double[,] MatrixA)
+        public double[,] FindInitialFeasibleSolution(double[,] MatrixA)
         {
             int row = -1;
             int column = -1;
-            double pivot = 0;
+
+            int rowsCount = MatrixA.GetLength(0) - 1;
+            int colsCount = MatrixA.GetLength(1) - 1;
 
             bool IsComplete = false;
 
             
 
-            for (int i = 0; i < MatrixA.GetLength(0)-1; i++)
+            for (int i = 0; i < rowsCount; i++)
             {
-                if (MatrixA[i, MatrixA.GetLength(1)-1] < 0)
+                if (MatrixA[i, colsCount] < 0)
                 {
                     row = i;
                     IsComplete = true;
-                    ;
+                    break;
                 }                
             }
 
@@ -46,41 +52,205 @@ namespace JordanExceptions
             if (IsComplete)
             {
                 IsComplete = false;
-                
-                for (int j = 0; j < MatrixA.GetLength(1)-1; j++)
+
+                for (int j = 0; j < colsCount; j++)
                 {
                     if (MatrixA[row, j] < 0)
                     {
                         column = j;
                         IsComplete = true;
-                        continue;
+                        break;
                     }
                 }
 
 
                 if (IsComplete)
                 {
+                    row = FindMinPositiveRatio(MatrixA, column);
+                    double pivot = MatrixA[row, column];
 
+                    _protocol.SaveStepHeader(_rows[row], _columns[column], "Пошук опорного розв'язку");
 
-
-
-
+                    (_columns[column], _rows[row]) = (_rows[row], _columns[column]);
                     MatrixA = _jordan.MatrixSolver(MatrixA, pivot, row, column);
+
+
+                    _protocol.SaveTable(MatrixA, _rows, _columns);
+                    _protocol.SaveSectionHeader("ОПОРНИЙ РОЗВ'ЯЗОК: ");
+                    double[] resX = GenerateResult(MatrixA);
+                    _protocol.ResultSimplexSave(resX);
+
+
+                    return MatrixA;
+
                 }
                 else
                 {
                     throw new Exception("Система обмежень ЛП-задачі суперечлива");
                 }
-                   
-                            
+
+
             }
-        }                 
+            else 
+            {
+                _protocol.SaveStepHeader(_rows[row], _columns[column], "Пошук опорного розв'язку");
+                _protocol.SaveTable(MatrixA, _rows, _columns);
+                _protocol.SaveSectionHeader("ОПТИМАЛЬНИЙ РОЗВ'ЯЗОК: ");
+                double[] resX = GenerateResult(MatrixA);
+                _protocol.ResultSimplexSave(resX);
+
+                return MatrixA; 
+            }
+        }        
+        
           
         
 
-        public void FindOptimalSolution()
+
+
+
+        public double[] FindOptimalSolution(double[,] MatrixA)
         {
 
+            int rowsCount = MatrixA.GetLength(0) - 1;
+            int colsCount = MatrixA.GetLength(1) - 1;
+
+            InitializeLabels(rowsCount, colsCount);
+            _protocol.InputTableSave(MatrixA, _rows, _columns);
+
+
+            MatrixA = FindInitialFeasibleSolution(MatrixA);
+
+
+            bool optimal = false;
+
+            while (!optimal)
+            {
+                optimal = true;
+                for (int j = 0; j < colsCount; j++)
+                {
+                    if (MatrixA[rowsCount, j] < 0)
+                    {
+                        int row = FindMinPositiveRatio(MatrixA, j);
+                        if (row < 0) throw new Exception("Функція не має максимуму");
+
+                        _protocol.SaveStepHeader(_rows[row], _columns[j], "Пошук оптимального розв'язку");
+
+                        MatrixA = _jordan.MatrixSolver(MatrixA, MatrixA[row, j], row, j);
+                        (_columns[j], _rows[row]) = (_rows[row], _columns[j]);
+
+                        _protocol.SaveTable(MatrixA, _rows, _columns);
+
+                        optimal = false;
+                        break;
+                    }
+                }
+            }
+
+            double[] resX = GenerateResult(MatrixA);
+            _protocol.SaveSectionHeader("ОПТИМАЛЬНИЙ РОЗВ'ЯЗОК: ");
+            _protocol.ResultSimplexSave(resX, MatrixA[rowsCount, colsCount]);
+
+            return resX; ;
+        }
+
+
+
+        public int FindMinPositiveRatio(double[,] MatrixA, int column)
+        {
+            
+            double temp = 0;
+            double pivot = double.MaxValue;
+            int row = -1;
+
+            int rowsCount = MatrixA.GetLength(0) - 1;
+            int colsCount = MatrixA.GetLength(1) - 1;
+
+
+            for (int i = 0; i < rowsCount; i++)
+            {
+                if (pivot > 0)
+                {
+                    temp = pivot;
+                }
+
+
+                if (MatrixA[i, column] != 0)
+                {
+                    pivot = MatrixA[i, colsCount] / MatrixA[i, column];
+
+                    if (MatrixA[i, colsCount] == 0 && MatrixA[i, column] > 0)
+                    {
+                        row = i;
+                        break;
+                    }
+
+                }
+
+
+
+                if (pivot > 0 && pivot < temp)
+                {
+                    temp = pivot;
+                    row = i;
+                }
+            }
+            return row;
+        }
+
+
+
+
+        public void InitializeLabels(int rows, int cols)
+        {
+
+            _rows = new string[rows+1];
+
+            for (int i = 0; i < rows; i++)
+            {
+                _rows[i] = "y" + (i + 1);
+            }
+
+            _rows[rows] = "Z";
+            
+            _columns = new string[cols+1];
+            for (int j = 0; j < cols; j++)
+            {
+                _columns[j] = "x" + (j + 1);
+            }
+            _columns[cols] = "1";           
+        }
+
+
+        private double[] GenerateResult(double[,] MatrixA)
+        {
+            int rowsCount = MatrixA.GetLength(0) - 1;
+            int colsCount = MatrixA.GetLength(1) - 1;
+
+            double[] x = new double[colsCount];
+
+
+            for (int i = 0; i < rowsCount; i++)
+            {
+                string name = _rows[i];
+
+                if (name.StartsWith("x"))
+                {
+                    string numberPart = name.Substring(1);
+
+                    if (int.TryParse(numberPart, out int index))
+                    {
+                        int arrayIndex = index - 1;
+
+                        if (arrayIndex >= 0 && arrayIndex < x.Length)
+                        {
+                            x[arrayIndex] = MatrixA[i, colsCount];
+                        }
+                    }
+                }
+            }
+
+            return x;
         }
     }
 }
---
+++ Lab01(PartB)/JordanExceptions/TestProject/SimplexSolverTest.cs
@@ -0,0 +1,40 @@
+﻿using JordanExceptions;
+
+namespace TestProject
+{
+    public class SimplexSolverTest
+    {
+        [Theory]
+        [InlineData(new double[] {1,1,-1,-2,6,-1,-1,-1,1,-5,2,-1,3,4,10,-1,-2,1,1,0  }, 4, 5, new double[] { 0,22,0,8 }, 36)]
+        public void GetSolution(double[] Matrix, int r, int c, double[] expectedX, int expectedResult)
+        {
+            var _jordan = new JordanMethod();
+            var _protocol = new SaveProtocol();
+            var _simplex = new SimplexSolver(_protocol, _jordan);
+            double R = 0;
+            _protocol.FileCleaner();
+
+            double[,] inputMatrix = new double[r, c];
+            for (int i = 0; i < r; i++)
+                for (int j = 0; j < c; j++)
+                    inputMatrix[i, j] = Matrix[i * c + j];
+
+
+            double[] X = _simplex.FindOptimalSolution(inputMatrix);
+
+            for (int i = 0; i < X.Length; i++)
+            {
+                R += X[i] * -inputMatrix[r-1,i];
+            }
+            
+
+            for (int i = 0; i < X.Length; i++)
+            {
+                Assert.Equal(expectedX[i], X[i], precision: 2);
+            }
+           
+            Assert.Equal(expectedResult, R, precision: 2);
+
+        }
+    }
+}
