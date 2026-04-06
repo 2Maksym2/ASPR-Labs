a1193c3d123b217890d09705b00cff8b7bac1585
2Maksym2 <149327980+2Maksym2@users.noreply.github.com>
2026-04-07 01:09:10 +0300

    AddGomorySimplex

--- Lab01(PartD)/JordanExceptions/JordanExceptions/GomorySimplex.cs
+++ Lab01(PartD)/JordanExceptions/JordanExceptions/GomorySimplex.cs
@@ -1,35 +1,134 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace JordanExceptions
 {
-    public class GomorySimplex
+    public class GomorySimplex : SimplexSolver
     {
-        public GomorySimplex() { }
-
-
-        public double[] Solver()
+        private readonly ISaveProtocol _protocol;
+        public GomorySimplex(ISaveProtocol protocol, IJordanMethod jordan)
+        : base(protocol, jordan)
         {
-            throw new NotImplementedException();
+            _protocol = protocol;
+        }
+
+        public double[] Solver(double[,] MatrixA)
+        {
+            Rows = null;
+            Columns = null;
+            LastMatrix = null;
+
+            _protocol.SaveSectionHeader("ЦІЛОЧИСЕЛНЕ РОЗВ'ЯЗАННЯ: ");
+
+
+            int row = 0;
+            double epsilon = 1e-9;
+            while (true)
+            {
+                double[] fullSolution = FindOptimalSolution(MatrixA);
+                double[] x = fullSolution.Take(fullSolution.Length - 1).ToArray();
+
+
+
+                MatrixA = LastMatrix;
+
+                for (int i = 0; i < x.Length-1; i++)
+                {
+                    if (Math.Abs(x[i] - Math.Round(x[i])) > epsilon)
+                    {
+                        double[] Constraints = GenerateGomoryCut(FindMaxFractionalRow(x));
+                        MatrixA = AddConstraintToTable(MatrixA, Constraints);
+                        continue;
+                    }
+                    else return fullSolution;
+                }
+
+
+            }                        
         }
 
 
-        public int FindMaxFractionalRow()
+        public int FindMaxFractionalRow(double[] x)
         {
-            throw new NotImplementedException();
+            int row = 0;
+            double temp = double.MinValue;
+           
+            for (int i = 0; i < x.Length-1; i++)
+            {
+                if (x[i]%1 > temp)
+                {
+                    temp = x[i] % 1;
+                    row = Array.FindIndex(Rows, x => x == "x" + (i+1));
+                }
+            }
+
+
+            return row;
         }
 
-        public double[] GenerateGomoryCut()
+        public double[] GenerateGomoryCut(int row)
         {
-            throw new NotImplementedException();
+            int cols = LastMatrix.GetLength(1);
+            double[] Constraints = new double[cols];
+
+
+            for (int i = 0; i < cols; i++)
+            {
+                double val = LastMatrix[row, i];
+                Constraints[i] = -(val - Math.Floor(val));
+            }
+
+            return Constraints;
+
         }
 
-        public double[,] AddConstraintToTable()
+        public double[,] AddConstraintToTable(double[,] MatrixA, double[] Constraints)
         {
-            throw new NotImplementedException();
+
+            int rows = LastMatrix.GetLength(0);
+            int cols = LastMatrix.GetLength(1);
+
+            double[,] newMatrix = new double[rows+1, cols];
+
+            for (int i = 0; i < rows + 1; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    if (i < rows-1)
+                    {
+                        newMatrix[i, j] = LastMatrix[i, j];
+                    }
+
+                    else if (i == rows-1)
+                    {
+                        newMatrix[i, j] = Constraints[j];
+                    }
+
+                    else
+                    {
+                        newMatrix[i, j] = LastMatrix[i - 1, j];
+                    }
+                }
+            }
+
+            string[] newLabels = new string[rows + 1];
+
+            for (int i = 0; i < rows - 1; i++)
+            {
+                newLabels[i] = Rows[i];
+            }
+
+            newLabels[rows - 1] = "S";
+
+            newLabels[rows] = "Z";
+
+            Rows = newLabels;
+            return newMatrix;
         }
     }
 }
